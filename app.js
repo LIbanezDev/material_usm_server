76091a8d64d3a64ac257f41c272ab22789cb1698
lucas <lucas.vergara@usm.cl>
2020-08-14 19:40:19 -0400

    Agregar configuración de multer

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
 const cors = require('cors')
 app.use(cors())
 const bodyParser = require('body-parser')
 
-// Export io before importing routes, otherwise this shit doesn't works
+// General config file uploads
+const multer = require('multer')
+const multerMid = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        // no larger than 5mb.
+        fileSize: 5 * 1024 * 1024,
+    },
+});
+app.disable('x-powered-by')
+app.use(multerMid.single('archivo_exe'))
+
+// Export io before importing routes that use it.
 module.exports = io
 
 require('./sockets/sockets')
-const arduinoRoutes = require('./routes/arduino')
-const userRoutes = require('./routes/users')
+const userRoutes = require('./routes/users.routes')
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
@@ -20,8 +30,6 @@ app.use(bodyParser.urlencoded({extended: false}))
 // parse application/json
 app.use(bodyParser.json())
 
-
-app.use('/arduino', arduinoRoutes)
 app.use('/api/users', userRoutes)
 
 const port = 4000
