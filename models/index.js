470ee780685bdcbd905e6014ede6f78415862dab
lucas <lucas.vergara@usm.cl>
2020-08-14 19:41:28 -0400

    Agregar sequelize authenticate para checkear conexion a base de datos al iniciar servidor

--- models/index.js
+++ models/index.js
@@ -8,6 +8,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+// Connect to db
 let sequelize;
 if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -15,8 +16,13 @@ if (config.use_env_variable) {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-    .readdirSync(__dirname)
+// Check connection
+sequelize.authenticate()
+    .then(() => console.log('Conexion exitosa a la base de datos'))
+    .catch(err => console.log(`Error al conectar a la base de datos - error = ${err}`))
+
+// Get models
+fs.readdirSync(__dirname)
     .filter(file => {
         return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
     })
@@ -25,6 +31,7 @@ fs
         db[model.name] = model;
     });
 
+// Associate models
 Object.keys(db).forEach(modelName => {
     if (db[modelName].associate) {
         db[modelName].associate(db);
